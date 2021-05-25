96dda82ec2f2928bbbef693a095290b1904f1924
dynamo <hietanenelias@gmail.com>
2021-05-25 16:12:03 +0200

    Création de la classe ClassicTextWrapper

---
+++ src/main/java/org/typ/model/ClassicTextWrapper.java
@@ -0,0 +1,34 @@
+package org.typ.model;
+
+import java.util.List;
+
+public class ClassicTextWrapper implements TextWrapper{
+
+    /**
+     * Liste qui contient tous les mots d'un texte
+     */
+    private List<String> wordList;
+
+    public ClassicTextWrapper(List<String> wordList){
+        this.wordList = wordList;
+    }
+
+    /**
+     * retourne le mot l'indice passé en paramètre dans la ltiste
+     * @param position indice du mot que l'on veut retourner
+     * @return mot à l'indice passé en parmaètre
+     */
+    @Override
+    public String getWord(int position) {
+        return this.wordList.get(position);
+    }
+
+    /**
+     * retourne la liste contenant les mots du texte
+     * @return liste contenant les mots du texte
+     */
+    @Override
+    public List<String> getText() {
+        return this.wordList;
+    }
+}
