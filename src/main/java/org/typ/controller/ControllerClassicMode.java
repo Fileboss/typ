956ca61cc8bbf6dc116ef459238fe905c9b2e7c1
philippe <bausphilippe@gmail.com>
2021-06-01 16:47:32 +0200

    Now, we use fxml

--- src/main/java/org/typ/controller/ControllerClassicMode.java
+++ src/main/java/org/typ/controller/ControllerClassicMode.java
@@ -3,11 +3,13 @@ package org.typ.controller;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DialogEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.typ.model.ClassicCorrector;
@@ -17,9 +19,14 @@ public class ControllerClassicMode extends VBox {
 
 
     private ClassicCorrector model;
-    private HBox hBoxQuitterRejouer;
+
+    @FXML
     private TextField textInput;
+
+    @FXML
     private Button exitButton;
+
+    @FXML
     private Button replayButton;
 
     /**
@@ -29,69 +36,11 @@ public class ControllerClassicMode extends VBox {
     public ControllerClassicMode(ClassicCorrector model) {
         super(20);
         this.model = model;
-        this.model.start();
-        /* Redéfinition du textfield pour refuser les espaces */
-        textInput = new TextField() {
-            @Override public void replaceText(int start, int end, String text) {
-                if (!text.matches("\\s")) {
-                    super.replaceText(start, end, text);
-                }
-            }
-            @Override public void replaceSelection(String text) {
-                if (!text.matches("\\s")) {
-                    super.replaceSelection(text);
-                }
-            }
-        };
-
-        /* Définition du comportement lorsque la barre espace est enfoncée. (validation du mot) */
-        textInput.setOnKeyPressed(e -> {
-            //System.out.println("released : "+e.getCode());
-            KeyCode keyPressed = e.getCode();
-            if (keyPressed == KeyCode.SPACE && !textInput.getText().isEmpty() && !model.isGameOver()) {
-                try {
-                    model.evaluateWord(textInput.getText());
-                    textInput.setText("");
-                    model.nextWord();
-                } catch (GameOverException gameOverException) {
-                    textInput.setEditable(false);
-
-                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                    alert.setTitle("Exception");
-                    alert.setHeaderText(null);
-                    alert.setContentText(gameOverException.getMessage());
-
-                    EventHandler<DialogEvent> event =
-                            e1 -> onClickReplayButton(null);
-
-                    alert.setOnCloseRequest(event);
-                    alert.showAndWait();
-                }
-            }
-            if (model.isGameOver()){
-                textInput.setEditable(false);
-
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Game Over");
-                alert.setHeaderText(null);
-                alert.setContentText("Fin de la partie");
-
-                EventHandler<DialogEvent> event =
-                        e1 -> onClickReplayButton(null);
-                alert.setOnCloseRequest(event);
-                alert.showAndWait();
-            }
-        });
 
         // Définition du comportement lors d'un changement sur le textInput. (validation par caractère)
-        textInput.textProperty().addListener(this::validateCharacters);
 
-        exitButton = new Button("Exit");
-        replayButton = new Button("Replay");
-        replayButton.setOnAction(this::onClickReplayButton);
-        exitButton.setOnAction(this::onClickExitButton);
-        hBoxQuitterRejouer = new HBox(500, exitButton, replayButton);
-        this.getChildren().addAll(textInput, hBoxQuitterRejouer);
+        // TODO
+        //textInput.textProperty().addListener(this::validateCharacters);
 
     }
 
@@ -104,6 +53,7 @@ public class ControllerClassicMode extends VBox {
      * Méthode invoquée lors du click sur le bouton "quitter". Ferme l'application.
      * @param e
      */
+    @FXML
     private void onClickExitButton(ActionEvent e) {
         System.exit(1);
     }
@@ -113,6 +63,7 @@ public class ControllerClassicMode extends VBox {
      * Réinitialise la partie.
      * @param e
      */
+    @FXML
     private void onClickReplayButton(ActionEvent e) {
 
         model.initialize();
@@ -122,4 +73,46 @@ public class ControllerClassicMode extends VBox {
 
     }
 
+    @FXML
+    public void onSpacePressed(KeyEvent e) {
+        KeyCode keyPressed = e.getCode();
+        if (keyPressed == KeyCode.SPACE && !textInput.getText().isEmpty() && !model.isGameOver()) {
+            try {
+                System.out.println(textInput.getText());
+                model.evaluateWord(textInput.getText());
+                textInput.setText("");
+                model.nextWord();
+            } catch (GameOverException gameOverException) {
+                textInput.setEditable(false);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Exception");
+                alert.setHeaderText(null);
+                alert.setContentText(gameOverException.getMessage());
+
+                EventHandler<DialogEvent> event =
+                        e1 -> onClickReplayButton(null);
+
+                alert.setOnCloseRequest(event);
+                alert.showAndWait();
+            }
+        }
+        if (model.isGameOver()){
+            textInput.setEditable(false);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Game Over");
+            alert.setHeaderText(null);
+            alert.setContentText("Fin de la partie");
+
+            EventHandler<DialogEvent> event =
+                    e1 -> onClickReplayButton(null);
+            alert.setOnCloseRequest(event);
+            alert.showAndWait();
+        }
+    }
+
+    public void start() {
+        textInput.textProperty().addListener(this::validateCharacters);
+    }
 }
