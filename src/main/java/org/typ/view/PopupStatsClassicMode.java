91f62aab2a3257da0fd091124cccab5cfdfef394
cedric.olender <cedric.olender@gmail.com>
2021-06-14 11:38:02 +0200

    [FT-16] Mise en oeuvre des stats classique
    fix l'affichage de temps à la fin

--- src/main/java/org/typ/view/PopupStatsClassicMode.java
+++ src/main/java/org/typ/view/PopupStatsClassicMode.java
@@ -1,9 +1,10 @@
 package org.typ.view;
 
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
 import org.typ.model.Statistics;
 import org.typ.model.TimedStatistics;
+
+import java.text.SimpleDateFormat;
 
 public class PopupStatsClassicMode extends Alert {
     private String timeLabel;
@@ -18,14 +19,16 @@ public class PopupStatsClassicMode extends Alert {
 
     public PopupStatsClassicMode(Statistics stats) {
         super(Alert.AlertType.INFORMATION);
-        this.timeLabel = new String("Temps : " + ((TimedStatistics)stats).getTime());
+        int time = ((TimedStatistics)stats).getTime();
+        String formattedTime = new SimpleDateFormat("mm:ss").format(time * 1000);
+        this.timeLabel = new String("Temps : " + formattedTime.toString());
         this.nbCorrectWordsLabel = new String("Correct words : " + stats.getNbCorrectWords());
         this.nbIncorrectWordsLabel = new String("Incorrect words : " + stats.getNbIncorrectWords());
         this.nbCorrectInputsLabel = new String("Correct inputs : " + stats.getNbCorrectInputs());
         this.nbIncorrectInputsLabel = new String("Incorrect inputs : " + stats.getNbIncorrectInputs());
 
         StringBuilder builder = new StringBuilder();
-        builder.append(timeLabel + "s\n");
+        builder.append(timeLabel + "\n");
         builder.append(nbCorrectWordsLabel + "\n");
         builder.append(nbIncorrectWordsLabel + "\n");
         builder.append(nbCorrectInputsLabel + "\n");
